ecadfd0065912cb6cd0f163a1155db400462d2a9
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2020-07-27 19:25:40 +0200

    wersja finalna (monolitycznie)

--- GraZgadywanka/ConsoleAppMonolitycznie/Program.cs
+++ GraZgadywanka/ConsoleAppMonolitycznie/Program.cs
@@ -20,31 +20,58 @@ namespace ConsoleAppMonolitycznie
             Console.WriteLine("Wylosowałem liczbę od 1 do 100.\nOdgadnij ją!");
             #endregion
 
-            Console.Write("Podaj swoją propozycję: ");
-            //2. Człowiek proponuje wartość
-            int propozycja = int.Parse(Console.ReadLine());
+            bool odgadniete = false;
+            do  //powtarzaj
+            {
+                //2. Człowiek proponuje wartość
+                Console.Write("Podaj swoją propozycję: ");
+                int propozycja;
+                try
+                {
+                    propozycja = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Nie podano poprawnej liczby. Koniec programu");
+                    return;
+                }
+                catch(OverflowException)
+                {
+                    Console.WriteLine("Podano za dużą liczbę. Koniec programu");
+                    return;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Niezindentyfikowana awaria. Koniec programu");
+                    return;
+                }
 
-            //3. Komputer ocenia propozycję człowieka
-            #region komputer ocenia
-            if (propozycja < wylosowana)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Za mało");
-                Console.ResetColor();
+                //3. Komputer ocenia propozycję człowieka
+                #region komputer ocenia
+                if (propozycja < wylosowana)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Za mało");
+                    Console.ResetColor();
+                }
+                else if (propozycja > wylosowana)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Za dużo");
+                    Console.ResetColor();
+                }
+                else //propozycja == wylosowana
+                {
+                    odgadniete = true;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Trafiłeś");
+                    Console.ResetColor();
+                }
+                #endregion
             }
-            else if (propozycja > wylosowana)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Za dużo");
-                Console.ResetColor();
-            }
-            else //propozycja == wylosowana
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Trafiłeś");
-                Console.ResetColor();
-            }
-            #endregion
+            while (!odgadniete); // dopóki nie odgadnięto
+
+            Console.WriteLine("Koniec gry");
         }
     }
 }
